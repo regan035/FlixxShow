f3ac616a43f45cf18da8fd9c92053ff8c06b9f1d
gen <regan035@gmail.com>
2023-11-13 16:36:39 -0600

    fetch api

--- js/script.js
+++ js/script.js
@@ -1,6 +1,53 @@
 const currentPage = window.location.pathname;
 console.log(currentPage);
 
+//Fetch data from TMDB server
+const fetchApiData = async (endpoint) => {
+  const api_key = "86104454e33bd2e857294e8a7e7b45db";
+  const api_url = "https://api.tmdb.org/3/";
+
+  const response = await fetch(
+    `${api_url}${endpoint}?api_key=${api_key}&language=en-US`
+  );
+
+  const data = await response.json();
+  return data;
+};
+
+//display popular movies
+const displayPopoularMovies = async () => {
+  // use distructure to convert result from objects to arrays
+  const { results } = await fetchApiData("movie/popular");
+  console.log(results);
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+    <a href="movie-details.html?id=${movie.id}">
+    ${
+      movie.poster_path
+        ? `<img
+    src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+    class="card-img-top"
+    alt="${movie.title}"
+  />`
+        : `<img
+  src="images/no-image.jpg"
+  class="card-img-top"
+  alt="Movie Title"
+/>`
+    }
+  </a>
+  <div class="card-body">
+    <h5 class="card-title">${movie.title}</h5>
+    <p class="card-text">
+      <small class="text-muted">Release: ${movie.release_date}</small>
+    </p>
+  </div>`;
+    document.querySelector("#popular-movies").appendChild(div);
+  });
+};
+
 //high light active links
 const highlightActiveLinks = () => {
   const links = document.querySelectorAll(".nav-link");
@@ -15,7 +62,7 @@ const init = () => {
   switch (currentPage) {
     case "/":
     case "/index.html":
-      console.log("Home");
+      displayPopoularMovies();
       break;
     case "/show.html":
       console.log("Shows");
