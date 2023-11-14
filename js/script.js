1def960074a31114de4b39ec141cd72808256cb9
gen <regan035@gmail.com>
2023-11-13 21:50:55 -0600

    display movie details

--- js/script.js
+++ js/script.js
@@ -93,6 +93,77 @@ const displayPopoularShows = async () => {
     document.querySelector("#popular-shows").appendChild(div);
   });
 };
+
+//Display movie details
+const displayMovieDetails = async () => {
+  const movieId = window.location.search.split("=")[1];
+  console.log(movieId);
+
+  const movie = await fetchApiData(`movie/${movieId}`);
+  console.log(movie.production_companies);
+  const div = document.createElement("div");
+  div.innerHTML = `
+  <div class="details-top">
+  <div>
+  ${
+    movie.poster_path
+      ? `<img
+  src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+  class="card-img-top"
+  alt="${movie.title}"
+/>`
+      : `<img
+src="images/no-image.jpg"
+class="card-img-top"
+alt="Movie Title"
+/>`
+  }
+  </div>
+  <div>
+    <h2>${movie.title}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${movie.vote_average.toFixed(1)}/ 10
+    </p>
+    <p class="text-muted">Release Date: ${movie.release_date}</p>
+    <p>
+    ${movie.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+      ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+    </ul>
+    <a href="${
+      movie.homepage
+    }" target="_blank" class="btn">Visit Movie Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Movie Info</h2>
+  <ul>
+    <li><span class="text-secondary">Budget:</span> $${formNum(
+      movie.budget
+    )}</li>
+    <li><span class="text-secondary">Revenue:</span> $${formNum(
+      movie.revenue
+    )}</li>
+    <li><span class="text-secondary">Runtime:</span> ${movie.runtime}min</li>
+    <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">
+      
+      ${movie.production_companies.map((company) => `${company.name}`)}
+  </div>
+</div>
+  `;
+  document.querySelector("#movie-details").appendChild(div);
+};
+
+// add commas to number
+const formNum = (number) => {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
 //high light active links
 const highlightActiveLinks = () => {
   const links = document.querySelectorAll(".nav-link");
@@ -113,7 +184,7 @@ const init = () => {
       displayPopoularShows();
       break;
     case "/movie-details.html":
-      console.log("Movie Details");
+      displayMovieDetails();
       break;
     case "/tv-details.html":
       console.log("TV Details");
