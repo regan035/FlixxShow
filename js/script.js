b47e05de3cc0e7182b2b146d99230d8739346325
gen <regan035@gmail.com>
2023-11-14 18:20:21 -0600

    add tv detail page

--- js/script.js
+++ js/script.js
@@ -161,6 +161,74 @@ alt="Movie Title"
   document.querySelector("#movie-details").appendChild(div);
 };
 
+//Display tvshow details
+const displayshowDetails = async () => {
+  const showId = window.location.search.split("=")[1];
+  const show = await fetchApiData(`tv/${showId}`);
+  console.log(show);
+  //display background image in detail page
+  displayBackgroundImage("tv", show.backdrop_path);
+  const div = document.createElement("div");
+  div.innerHTML = `
+  <div class="details-top">
+  <div>
+  ${
+    show.poster_path
+      ? `<img
+  src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+  class="card-img-top"
+  alt="${show.name}"
+/>`
+      : `<img
+src="images/no-image.jpg"
+class="card-img-top"
+alt="show name"
+/>`
+  }
+  </div>
+  <div>
+    <h2>${show.name}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${show.vote_average.toFixed(1)}/ 10
+    </p>
+    <p class="text-muted">First Air Date: ${show.first_air_date}</p>
+    <p>
+    ${show.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+      ${show.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+    </ul>
+    <a href="${
+      show.homepage
+    }" target="_blank" class="btn">Visit show Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>show Info</h2>
+  <ul>
+    <li><span class="text-secondary">Number of Episodes:</span> ${
+      show.number_of_episodes
+    }</li>
+    <li><span class="text-secondary">Origin Country:</span> ${
+      show.origin_country
+    }</li>
+    <li><span class="text-secondary">Languages:</span> ${show.languages
+      .map((language) => language.toUpperCase())
+      .join("")}</li>
+    <li><span class="text-secondary">Status:</span> ${show.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">
+      
+      ${show.production_companies.map((company) => `${company.name}`)}
+  </div>
+</div>
+  `;
+  document.querySelector("#show-details").appendChild(div);
+};
+
 //diplay overlay background image
 const displayBackgroundImage = (type, backroundPath) => {
   const backgroundDiv = document.createElement("div");
@@ -210,7 +278,7 @@ const init = () => {
       displayMovieDetails();
       break;
     case "/tv-details.html":
-      console.log("TV Details");
+      displayshowDetails();
       break;
     case "/search.html":
       console.log("Search");
